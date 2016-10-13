b4acb847515cb5d6df2cd520f7024631ac702d4a
lklic <lklic@itatti.harvard.edu>
2016-10-13 17:28:05 +0200

    homepage updates

--- common/header.php
+++ common/header.php
@@ -73,16 +73,61 @@ fire_plugin_hook('public_header');
 ?>
 		</header>
 		
-
+  <!-- wrapper for header image starts, closed in footer, but can be closed in page as well and reopened -->
 	<div class="headWrapper">
-	<h1 id="site-title"><a href="#"><?php
+
+		
+		
+		
+		
+		  <!-- start main menu -->
+		<div id= mainMenu>
+			
+		
+			<nav class="top-bar" data-topbar role="navigation">
+  <ul class="title-area">
+    <li class="name">
+      <h1><a href="#"><?php
 echo link_to_home_page(theme_logo());
-?></a></h1>
-	
+?>
+		  </a></h1>
+    </li>
+     <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+    <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+  </ul>
 
-	
-	
-	
+  <section class="top-bar-section">
+    <!-- Right Nav Section -->
+    <ul class="right">
+      <li class="has-dropdown">
+        <a href="#">the rescue</a>
+        <ul class="dropdown">
+          <li><a href="#">First link in dropdown</a></li>
+          <li><a href="#">Second link in dropdown</a></li>
+        </ul>
+      </li>
+    </ul>
 
+    <!-- Left Nav Section -->
+    <ul class="left">
+      <li class="has-dropdown">
+        <a href="#">the committee</a>
+        <ul class="dropdown">
+          <li><a href="#">First link in dropdown</a></li>
+          <li><a href="#">Second link in dropdown</a></li>
+        </ul>
+      </li>
+    </ul>
+
+  </section>
+</nav>
+			
+			
+			
+ </div> 
+	
+	
+	
+		  <!-- open up these divs to center the content from all pages, and set max width -->
 	<div class="row">
 		<div class="large-12 columns">
