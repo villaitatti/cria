2c3b952d3e3f47e8438321d7105be635e80e6196
Andrea Caselli <acaselli@itatti.harvard.edu>
2016-10-16 17:58:36 +0200

    Added exhibit template files and fixed the next and previous item from an exhibit

--- items/show.php
+++ items/show.php
@@ -6,8 +6,7 @@
         <!-- PREVIOUS AND NEXT ITEM -->
         <div class="div-prev-next-items">
             <ul class="item-pagination navigation">
-                <li id="previous-item" class="previous, previous-item" ><?php echo link_to_previous_item_show(); ?></li>
-                <li id="next-item" class="next, next-item" ><?php echo link_to_next_item_show(); ?></li>
+                <?php custom_paging(); ?>
             </ul>
         </div>
 
@@ -35,20 +34,56 @@
     <a href="#panel1a"><b style="color: #de171c">ITEM DATA</b></a>
     <div id="panel1a" class="content">
         <div>
+            <?php $title = metadata('item', array('Dublin Core', 'Title')); ?>
+    <?php if ($title): ?>
             <h6 class="accordion-item-titles">Title</h6>
-            <p><?php echo metadata('item', array('Dublin Core', 'Title')); ?></p>
+            <p><?php echo $title; ?></p>
+    <?php endif; ?>
+    <?php $box_number = metadata('item', array('Item Type Metadata', 'Box Number')); ?>
+    <?php if ($box_number): ?>
             <h6 class="accordion-item-titles">Box Number</h6>
-            <p><?php echo metadata('item', array('Item Type Metadata', 'Box Number')); ?></p>
+            <p><?php echo $box_number; ?></p>
+    <?php endif; ?>
+    <?php $folder_number = metadata('item', array('Item Type Metadata', 'Folder Number')); ?>
+    <?php if ($folder_number): ?>
             <h6 class="accordion-item-titles">Folder Number</h6>
-            <p><?php echo metadata('item', array('Item Type Metadata', 'Folder Number')); ?></p>
+            <p><?php echo $folder_number; ?></p>
+    <?php endif; ?>
+    <?php $image_number = metadata('item', array('Item Type Metadata', 'Image Number')); ?>
+    <?php if ($image_number): ?>
             <h6 class="accordion-item-titles">Image Number</h6>
-            <p><?php echo metadata('item', array('Item Type Metadata', 'Image Number')); ?></p>
+            <p><?php echo $image_number; ?></p>
+    <?php endif; ?>
+    <?php $folder_title = metadata('item', array('Item Type Metadata', 'Folder Title')); ?>
+    <?php if ($folder_title): ?>
             <h6 class="accordion-item-titles">Folder Title</h6>
-            <p><?php echo metadata('item', array('Item Type Metadata', 'Folder Title')); ?></p>
+            <p><?php echo $folder_title; ?></p>
+    <?php endif; ?>
+    <?php $photographer = metadata('item', array('Item Type Metadata', 'Photographer')); ?>
+    <?php if ($photographer): ?>
+            <h6 class="accordion-item-titles">Photographer</h6>
+            <p><?php echo $photographer; ?></p>
+    <?php endif; ?>
+    <?php $location = metadata('item', array('Item Type Metadata', 'Location')); ?>
+    <?php if ($location): ?>
             <h6 class="accordion-item-titles">Location</h6>
-            <p><?php echo metadata('item', array('Item Type Metadata', 'Location')); ?></p>
-            <h6 class="accordion-item-titles">Physical Dimensions</h6>
-            <p><?php echo metadata('item', array('Item Type Metadata', 'Physical Dimensions')); ?></p>
+            <p><?php echo $location; ?></p>
+    <?php endif; ?>
+    <?php $artist = metadata('item', array('Item Type Metadata', 'Artist')); ?>
+    <?php if ($artist): ?>
+            <h6 class="accordion-item-titles">Artist</h6>
+            <p><?php echo $artist; ?></p>
+    <?php endif; ?>
+    <?php $restorer = metadata('item', array('Item Type Metadata', 'Restorer')); ?>
+    <?php if ($restorer): ?>
+            <h6 class="accordion-item-titles">Restorer</h6>
+            <p><?php echo $restorer; ?></p>
+    <?php endif; ?>
+    <?php $physical_dimensions = metadata('item', array('Item Type Metadata', 'Physical Dimensions')); ?>
+    <?php if ($physical_dimensions): ?>
+            <h6 class="accordion-item-titles">Physical Dimension</h6>
+            <p><?php echo $physical_dimensions; ?></p>
+    <?php endif; ?>
         </div>
         
     </div>
