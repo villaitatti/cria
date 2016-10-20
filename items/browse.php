92aa0559152b74814d9ce88084f222e73c7e1367
Andrea Caselli <acaselli@itatti.harvard.edu>
2016-10-20 11:00:50 +0200

    Styled Browse items page

--- items/browse.php
+++ items/browse.php
@@ -1,5 +1,5 @@
 <?php
-$pageTitle = __('Browse Items');
+$pageTitle = __('All images');
 echo head(array('title'=>$pageTitle,'bodyclass' => 'items browse'));
 ?>
 
@@ -18,47 +18,36 @@ echo head(array('title'=>$pageTitle,'bodyclass' => 'items browse'));
 
 <?php
 $sortLinks[__('Title')] = 'Dublin Core,Title';
-$sortLinks[__('Creator')] = 'Dublin Core,Creator';
 $sortLinks[__('Date Added')] = 'added';
 ?>
-<div id="sort-links">
+<div id="sort-links" style="margin-top:4rem;">
     <span class="sort-label"><?php echo __('Sort by: '); ?></span><?php echo browse_sort_links($sortLinks); ?>
 </div>
 
 <?php endif; ?>
 
 <?php foreach (loop('items') as $item): ?>
-<div class="item hentry">
-    <h2><?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class'=>'permalink')); ?></h2>
+<div class="item hentry large-4 columns" style="margin-bottom:2rem;">
+    
     <div class="item-meta">
     <?php if (metadata('item', 'has thumbnail')): ?>
     <div class="item-img">
         <?php echo link_to_item(item_image('square_thumbnail')); ?>
     </div>
     <?php endif; ?>
-
-    <?php if ($description = metadata('item', array('Dublin Core', 'Description'), array('snippet'=>250))): ?>
-    <div class="item-description">
-        <?php echo $description; ?>
+    <div style="height:4.375rem;">
+        <h6><?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class'=>'permalink')); ?></h6>
     </div>
-    <?php endif; ?>
-
-    <?php if (metadata('item', 'has tags')): ?>
-    <div class="tags"><p><strong><?php echo __('Tags'); ?>:</strong>
-        <?php echo tag_string('items'); ?></p>
-    </div>
-    <?php endif; ?>
-
     <?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' =>$item)); ?>
 
     </div><!-- end class="item-meta" -->
 </div><!-- end class="item hentry" -->
 <?php endforeach; ?>
-
+<div>
 <?php echo pagination_links(); ?>
-
-
-
+</div>
+<div style="margin:2rem;">> </div>
 <?php fire_plugin_hook('public_items_browse', array('items'=>$items, 'view' => $this)); ?>
-
+<br/>
+<br/>
 <?php echo foot(); ?>
