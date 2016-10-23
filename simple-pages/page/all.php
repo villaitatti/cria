9ffaa11e92b63aaffe3bab5b961050bf04f6f9e8
Andrea Caselli <acaselli@itatti.harvard.edu>
2016-10-23 17:30:34 +0200

    All images grid mockup

--- simple-pages/page/all.php
+++ simple-pages/page/all.php
@@ -1,1 +1,51 @@
-<h1> testalsdhflkadhslajkdshlajhkds </h1>
+<?php
+$pageTitle = __('All images');
+echo head(array('title'=>$pageTitle,'bodyclass' => 'items browse'));
+?>
+
+<h1><?php echo $pageTitle;?> <?php echo __('(%s total)', $total_results); ?></h1>
+
+
+<nav class="items-nav navigation secondary-nav">
+    <?php echo public_nav_items(); ?>
+</nav>
+
+<?php echo item_search_filters(); ?>
+
+<?php echo pagination_links(); ?>
+
+<?php if ($total_results > 0): ?>
+
+<?php
+$sortLinks[__('Title')] = 'Dublin Core,Title';
+$sortLinks[__('Date Added')] = 'added';
+?>
+<div id="sort-links" style="margin-top:3.5rem;">
+    <span class="sort-label"><?php echo __('Sort by: '); ?></span><?php echo browse_sort_links($sortLinks); ?>
+</div>
+
+<?php endif; ?>
+<section id="photos">
+<?php foreach (loop('items') as $item): ?>
+    
+    <div class="item-meta">
+    <?php if (metadata('item', 'has thumbnail')): ?>
+    <div class="item-img">
+        <?php echo link_to_item(item_image('square_thumbnail')); ?>
+    </div>
+    <?php endif; ?>
+    <?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' =>$item)); ?>
+
+    </div><!-- end class="item-meta" -->
+<?php endforeach; ?>
+</section>
+
+<div>
+<?php echo pagination_links(); ?>
+</div>
+<?php if ($total_results > 12): ?>
+<div style="margin:2rem;">> </div>
+<?php endif; ?>
+<?php fire_plugin_hook('public_items_browse', array('items'=>$items, 'view' => $this)); ?>
+<br/>
+<br/>
